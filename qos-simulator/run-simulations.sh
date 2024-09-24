2a8581a9d3321087cc6563cb77ce2c107fed3975
houssamhh <houssamhajjhassan@gmail.com>
2024-09-24 11:30:14 +0200

    rename file

--- qos-simulator/run-simulations.sh
+++ qos-simulator/run-simulations.sh
@@ -6,5 +6,5 @@ do
 	echo "Running simulation for $line"
 	./qos-simulator.sh scenarios/real-traces-default.json qos-metrics.csv 300 "$i" $line
   	i=i+1
-done < scenarios/configurations.csv
+done < scenarios/mitigations.csv
 echo "Simulations done!"
