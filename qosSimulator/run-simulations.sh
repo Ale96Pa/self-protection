6ade1d2f667a31651fc34ab95dd2f6804edcda90
Alessandro Palma <palma.1871556@studenti.uniroma1.it>
2025-01-27 16:10:23 +0100

    readme

--- qosSimulator/run-simulations.sh
+++ qosSimulator/run-simulations.sh
@@ -4,7 +4,7 @@ i=0
 while IFS= read -r line
 do
 	echo "Running simulation for $line"
-	./qos-simulator.sh scenarios/healthcare-default.json qos-metrics-healthcare.csv 300 $line
+	./qosSimulator.sh scenarios/healthcare-default.json qos-metrics-healthcare.csv 300 $line
   	i=i+1
 done < scenarios/mitigations-healthcare.csv
 echo "Simulations done!"
